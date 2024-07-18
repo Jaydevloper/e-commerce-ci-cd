620299719a7a977c7cac376612359679af2dcafe
Jaydevloper <jayhanmirx@gmail.com>
2024-07-18 19:16:40 +0500

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      assets: path.resolve(__dirname, "./src/assets"),
+      components: path.resolve(__dirname, "./src/components"),
+      pages: path.resolve(__dirname, "./src/pages"),
+      modules: path.resolve(__dirname, "./src/modules/"),
+      hooks: path.resolve(__dirname, "./src/hooks/"),
+    },
+  },
+});
