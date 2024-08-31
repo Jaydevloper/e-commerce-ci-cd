ec3071888ffda8772d5756734bcefbe4fdb554d3
Jaydevloper <jayhanmirx@gmail.com>
2024-08-31 17:57:24 +0500

    edition:slideshow

--- src/modules/Home/components/Menu.tsx
+++ src/modules/Home/components/Menu.tsx
@@ -1,11 +1,40 @@
 import { Carousel } from "antd";
+import ArrowRightIcon from "assets/icons/arrow-right-icon";
 import "assets/scss/blocks/_bg-view.scss";
+import { Link } from "react-router-dom";
 const Menu = () => {
   return (
     <div>
       <div></div>
-      <Carousel>
-        <div className="bg-view bg-view__top-menu-ad bg-view__top-menu-ad-first"></div>
+      <Carousel autoplay>
+        <div className="bg-view bg-view__top-menu-ad bg-view__top-menu-ad-first text-white  ">
+          <div className="pl-[13rem] pt-32 max-w-[500px] w-[100%]">
+            <h2 className="text-5xl  font-semibold pb-[21px] leading-[1.5] ">
+              Up to 10% off Voucher
+            </h2>
+            <Link
+              className="text-base underline flex items-center gap-2 "
+              to={"/"}
+            >
+              Shop Now
+              <ArrowRightIcon />
+            </Link>
+          </div>
+        </div>
+        <div className="bg-view bg-view__top-menu-ad bg-view__top-menu-ad-second text-white">
+          <div className="pl-[13rem] pt-32 max-w-[500px] w-[100%]">
+            <h2 className="text-5xl  font-semibold pb-[21px] leading-[1.5] ">
+              Up to 10% off Voucher
+            </h2>
+            <Link
+              className="text-base underline flex items-center gap-2 "
+              to={"/"}
+            >
+              Shop Now
+              <ArrowRightIcon />
+            </Link>
+          </div>
+        </div>
       </Carousel>
     </div>
   );
